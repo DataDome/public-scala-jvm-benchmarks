4d47eeb20e1a2febf66efe1f42e46be4391effe2
Gaël Renoux <gael.renoux@datadome.co>
2025-10-01 14:30:39 +0200

    Fix script

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -127,33 +127,33 @@ if [ "$DRY_RUN" == "--dry-run" ]; then
 fi
 
 echo ""
-echo "+=====================+"
-echo "| [0/7] JVM Selection |"
-echo "+=====================+"
-. ./scripts/shell/select-jvm.sh
-
-echo ""
 echo "+=======================+"
-echo "| [1/7] Load Properties |"
+echo "| [1/8] Load Properties |"
 echo "+=======================+"
 . ./scripts/shell/load-properties.sh
 
 echo ""
+echo "+=====================+"
+echo "| [2/8] JVM Selection |"
+echo "+=====================+"
+. ./scripts/shell/select-jvm.sh
+
+echo ""
 echo "+================================+"
-echo "| [2/7] Load Architecture and OS |"
+echo "| [3/8] Load Architecture and OS |"
 echo "+================================+"
 . ./scripts/shell/configure-arch.sh
 . ./scripts/shell/configure-os.sh
 
 echo ""
 echo "+========================+"
-echo "| [3/7] JQ Configuration |"
+echo "| [4/8] JQ Configuration |"
 echo "+========================+"
 . ./scripts/shell/configure-jq.sh || exit 1
 
 echo ""
 echo "+===============================+"
-echo "| [4/7] Compile benchmark suite |"
+echo "| [5/8] Compile benchmark suite |"
 echo "+===============================+"
 if ! compile_benchmark_suite; then
   exit 1
@@ -161,18 +161,18 @@ fi
 
 echo ""
 echo "+=========================+"
-echo "| [5/7] JVM Configuration |"
+echo "| [6/8] JVM Configuration |"
 echo "+=========================+"
 . ./scripts/shell/configure-jvm.sh || exit 1
 
 echo ""
 echo "+=========================+"
-echo "| [6/7] JMH Configuration |"
+echo "| [7/8] JMH Configuration |"
 echo "+=========================+"
 . ./scripts/shell/configure-jmh.sh || exit 1
 
 echo ""
 echo "+===========================+"
-echo "| [7/7] Run benchmark suite |"
+echo "| [8/8] Run benchmark suite |"
 echo "+===========================+"
 run_benchmark_suite
